37cf435a589a414edf021d6a09c9350cafa594e3
Sebastian Kunert <skunert@trifacta.com>
2020-04-24 09:47:07 +0200

    Add package.json

--- phabricator-bitbar.js
+++ phabricator-bitbar.js
@@ -1,62 +1,87 @@
 #!/usr/bin/env PATH_TO_NODE_EXECUTABLE
 const config = {
+    // api token for conduit
     apiToken: null,
-    userPhid: null,
+    // your PHID
+    authors: null,
+    // PHIDs that should be used to query diffs you want to review
+    reviewers: null,
+    // host for you conduit api
     host: null
 };
 
+const bitbar = require('bitbar');
+const axios = require('axios');
+
+const queryDiff = async (extraParams) => {
+    return axios.get(`https://${config.host}/api/differential.query`, {
+        params: {
+            ...extraParams,
+            status: "status-open",
+            'api.token': config.apiToken
+        }
+    });
+}
+
+const getStatusIcon = (item) => {
+    if (item.statusName === "Accepted") {
+        return "✅";
+    } else if (item.statusName === "Needs Review") {
+        return "⏳";
+    } else {
+        return "❓"
+    }
+};
+
+const getBuildIcon = (item) => {
+    if (item.properties && item.properties.buildables) {
+        const buildables = Object.entries(item.properties.buildables);
+        let status = buildables[buildables.length - 1][1].status;
+        if (status === "passed") {
+            return "✅";
+        } else if (status === "failed") {
+            return ":x:";
+        }
+        return ` ${status} `
+    } else {
+        return "⏳";
+    }
+}
 
 (async () => {
-    const bitbar = require('bitbar');
-    const axios = require('axios');
-
-    if (!config.apiToken || !config.userPhid) {
+    if (!config.apiToken || !config.authors || config.authors.length === 0) {
         bitbar([{text: "Please edit phabricator-bitbar and add api-token and user-phid."}]);
         return;
     }
 
 
-    const result = await axios.get(`https://${config.host}/api/differential.query`, {
-        params: {
-            authors: [config.userPhid],
-            status: "status-open",
-            'api.token': config.apiToken
-        }
-    })
-    if (result.status !== 200) {
-        bitbar([{text: "Conduit Problem: received status " + result.status}]);
+    let queriesToWaitOn = [queryDiff({authors: config.authors})];
+    const shouldShowReviewDiffs = config.reviewers && config.reviewers.length > 0;
+
+    if (shouldShowReviewDiffs) {
+        queriesToWaitOn = queriesToWaitOn.concat(queryDiff({reviewers: config.reviewers}))
+    }
+
+    const [authorDiffResponse, diffsToBeReviewedResponse] = await Promise.all(queriesToWaitOn);
+
+    if (authorDiffResponse.status !== 200 || (shouldShowReviewDiffs && diffsToBeReviewedResponse.status !== 200)) {
+        bitbar([{text: `Conduit Problem: received status own diffs=${authorDiffResponse.status} reviews=${diffsToBeReviewedResponse.status}`}]);
         return;
     }
-    const getStatusSymbol = (item) => {
-        if (item.statusName === "Accepted") {
-            return "✅";
-        } else if (item.statusName === "Needs Review") {
-            return "⏳";
-        } else {
-            return "❓"
-        }
-    };
-    const getBuildSymbol = (item) => {
-        if (item.properties && item.properties.buildables) {
-            const buildables = Object.entries(item.properties.buildables);
-            let status = buildables[buildables.length - 1][1].status;
-            if (status === "passed") {
-                return "✅";
-            } else if (status === "failed") {
-                return ":x:";
-            }
-            return ` ${status} `
-        } else {
-            return "⏳";
-        }
-    }
-    let responseData = result.data.result.sort((i1, i2) => i1.id - i2.id);
-    const data = responseData.map(item => [{text: `${getStatusSymbol(item)} ${getBuildSymbol(item)} D${item.id} ${item.title}`, href: item.uri}]).flat();
-    const header = {text: responseData.map(item => `D${item.id} ${getStatusSymbol(item)}${getBuildSymbol(item)}`).join(' -- ')};
+
+    const authorDiffs = authorDiffResponse.data.result || [] ;
+    const reviewDiffs = (shouldShowReviewDiffs && diffsToBeReviewedResponse.data.result) || [];
+
+    const sortedAuthorDiffs = authorDiffs.sort((i1, i2) => i2.id - i1.id);
+    const header = {text: sortedAuthorDiffs.map(item => `D${item.id} ${getStatusIcon(item)}${getBuildIcon(item)}`).join(' -- ')};
+    const finalAuthorDiffs = sortedAuthorDiffs.map(item => [{text: `${getStatusIcon(item)}${getBuildIcon(item)} D${item.id} ${item.title}`, href: item.uri}]).flat();
+    const finalReviewDiffs = reviewDiffs.sort((i1, i2) => i2.id - i1.id).map(item => [{text: `D${item.id} ${item.title}`, href: item.uri}]).flat();
 
     bitbar([
         header,
         bitbar.separator,
-        ...data
+        ...finalAuthorDiffs,
+        bitbar.separator,
+        ...finalReviewDiffs
     ]);
 })();
